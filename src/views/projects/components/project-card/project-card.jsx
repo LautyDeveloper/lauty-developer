aab9bce3e8e07dd3fb666813e463df6d25229d16
Lautaro Aquino <laufabio24@gmail.com>
2025-09-08 16:46:52 -0300

    Mejoramos cartas de Proyectos.

--- src/views/projects/components/project-card/project-card.jsx
+++ src/views/projects/components/project-card/project-card.jsx
@@ -3,7 +3,17 @@ import "./project-card.css";
 export default function ProjectCard({ project, isLarge }) {
   return (
     <div className={isLarge ? "project-card large" : "project-card"}>
-      <img src={project.image} alt="Imagen del Proyecto" />
+      <div
+        className="project-image-container"
+        style={{
+          backgroundImage: `linear-gradient(
+          rgba(0, 0, 0, 0.5), 
+          rgba(0, 0, 0, 0.5)
+        ), url(${project.backImage})`,
+        }}
+      >
+        <img src={project.image} alt="Imagen del Proyecto" />
+      </div>
       <div className="project-card-text">
         <div className="project-title">
           <h3>{project.title}</h3>
--- src/views/projects/data/projects.js
+++ src/views/projects/data/projects.js
@@ -1,4 +1,12 @@
-// projects.js
+import back1 from "../media/projects-background/sec-project1.png";
+import back2 from "../media/projects-background/prim-project1.png";
+import back3 from "../media/projects-background/sec-project2.png";
+import back4 from "../media/projects-background/prim-project2.png";
+
+import project1 from "../media/projects-images/project-image2.png";
+import project2 from "../media/projects-images/project-image2.png";
+import project3 from "../media/projects-images/project-image2.png";
+import project4 from "../media/projects-images/project-image2.png";
 
 const projects = [
   {
@@ -6,23 +14,26 @@ const projects = [
     title: "BueloApp",
     description:
       "Aplicación web para la gestión de turnos médicos y notas familiares. Desarrollada con React y Node.js.",
-    image: "/images/bueloapp.png",
+    image: project1,
+    backImage: back1,
     link: "https://github.com/tuusuario/bueloapp",
   },
   {
     id: 2,
     title: "BueloApp",
     description:
-      "BueloApp es una plataforma web que ayuda a las familias a organizar turnos, medicaciones y notas para el cuidado de sus abuelos de forma simple y colaborativa.",
-    image: "/images/shoes-ecommerce.png",
+      "Aplicación web para la gestión de turnos médicos y notas familiares. Desarrollada con React y Node.js.",
+    image: project2,
+    backImage: back2,
     link: "https://github.com/tuusuario/shoes-ecommerce",
   },
   {
     id: 3,
     title: "S.G.D.P",
     description:
-    "El S.G.P.D es un sistema que organiza el préstamo de dispositivos en instituciones, registrando qué curso y profesor retira cada equipo para mantener un control claro y ordenado.",
-    image: "/images/crypto-dashboard.png",
+      "El S.G.P.D es un sistema que organiza el préstamo de dispositivos en instituciones, registrando qué curso y profesor retira cada equipo para mantener un control claro y ordenado.",
+    image: project3,
+    backImage: back3,
     link: "https://github.com/tuusuario/crypto-dashboard",
   },
   {
@@ -30,7 +41,8 @@ const projects = [
     title: "Portfolio 3D",
     description:
       "Portfolio personal con animaciones 3D utilizando Three.js y React. Incluye secciones de proyectos y contacto.",
-    image: "/images/portfolio-3d.png",
+    image: project4,
+    backImage: back4,
     link: "https://github.com/tuusuario/portfolio-3d",
   },
 ];
